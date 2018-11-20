c03ccfcb483419505137a612b03b782df55a025b
yinlisssss <38618580+yinlisssss@users.noreply.github.com>
2018-11-20 17:13:26 +0800

    Update LASSO.r

--- LASSO.r
+++ LASSO.r
@@ -1,29 +1,45 @@
 library("glmnet")
+
 library("survival")
+
 myexpr <- read.csv("easy_input_exp.csv",header = T,row.names = 1,check.names = F)
+
 mysurv <- read.csv("easy_input_suv.csv",header = T,row.names = 1)
-cvfit = cv.glmnet(t(myexpr), Surv(mysurv$futime,mysurv$fustat), nfold=10,
-                  family = "cox"
-) 
+
+cvfit = cv.glmnet(t(myexpr), Surv(mysurv$futime,mysurv$fustat), nfold=10, 
+                  family = "cox"   
+)  #LASSO regression with 10 folds cross validation
 plot(cvfit)
+
 fit <- glmnet(t(myexpr), Surv(mysurv$futime,mysurv$fustat), 
-              family = "cox")   #LASSO regression
+              family = "cox")   
 plot(fit, label = TRUE)
 
-cvfit$lambda.min #check lambda
+cvfit$lambda.min #check best lambda
 
 coef.min = coef(cvfit, s = "lambda.min") 
 coef.min
+
 active.min = which(coef.min != 0)
+
 geneids <- rownames(myexpr)[active.min]
-geneids
+
+geneids #check significant genes
 
 index.min = coef.min[active.min]
-index.min
+
+index.min #check index
+
 combine <- cbind(geneids, index.min)
+
 write.csv(combine,"gene_index.csv")
+
 signature <- as.matrix(t(myexpr[geneids,])) %*% as.matrix(index.min) 
+
 summary(signature)
+
 colnames(signature)[1] <- "lasso"
+
 row.names = row.names(myexpr)
+
 write.table(signature,"lasso_output.txt",row.names = T, quote = F)
